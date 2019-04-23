5fb4fc4be2562a42c580e984e03e77341b6f3fe1
Sean Lucey <Sean.Lucey@NOAA.gov>
2019-04-23 13:59:49 -0400

    Added loop to plot all scenarios

--- QNMcompare/QNM_MTI.R
+++ QNMcompare/QNM_MTI.R
@@ -23,6 +23,7 @@ GOM <- rpath(GOM.28b, "Gulf of Maine")
 
 #Run MTI
 gom.mti <- MTI(GOM, GOM.28b)
+gom.mti.down <- MTI(GOM, GOM.28b, increase = F)
 
 #Plot MTI results
 n.pred <- ncol(gom.mti)
@@ -53,67 +54,78 @@ for(igroup in 1:n.pred){
 }
 
 #Plot comparison plots
+scenarios <- c('Phytoplankton_up',
+               'Small crab other arthropoda_down',
+               'Mesozoop_up',
+               'Macrozoop_down',
+               'Megabenthos_up',
+               'Other pelagic mesopelagic_up',
+               'Small pelagics_up',
+               'D piscivores_up',
+               'D piscivores_down',
+               'Seals_up',
+               'Fishery_up',
+               'Fishery_down')
+decrease <- c(F, T, F, T, rep(F, 4), T, F, F, T)
+focal.group <- c(1, 8, 4, 6, 12, 16, 14, 17, 17, 22, 28, 28)
+
 #Gulf of Maine
-#Scenario 1 - Phytoplankton up
 out.order <- GOM.28b$model$Group
-gom.phyto.up <- data.table(Group = out.order, mti = gom.mti[, 1]) 
-
-#Load QPress results
-# out.order <- GOM.28b$model$Group
-# for(iscene in 1:5){
-#   qpress.results <- as.data.table(read.csv(file.path(qpress.dir, paste0(
-#     'GOMresults', iscene, 'Phytoplankton_up.csv'))))
-#   setnames(qpress.results, c('X', 'X.', 'X0', 'X..1'), c('Group', 'Neg', 'Neu', 'Pos'))
-#   
-#   #Convert to prop
-#   qpress.results[Pos > Neg, Value := Pos / 1000]
-#   qpress.results[Neg > Pos, Value := -1 * (Neg / 1000)]
-#   qpress.results[, out.order := factor(Group, levels = out.order)]
-#   setorder(qpress.results, out.order)
-#   
-#   scene.results <- qpress.results[, list(Value)]
-#   setnames(scene.results, 'Value', paste0('Model', iscene))
-#   
-#   gom.phyto.up <- cbind(gom.phyto.up, scene.results)
-# }
-
-for(iscene in 1:5){
-  qpress.results <- as.data.table(read.csv(file.path(qpress.dir, paste0(
-    'GOMresults', iscene, 'Phytoplankton_up.csv'))))
-  setnames(qpress.results, c('X', 'X.', 'X0', 'X..1'), c('Group', 'Neg', 'Neu', 'Pos'))
+for(iscene in 1:12){
+  if(decrease[iscene]){
+    scene.out <- data.table(Group = out.order, 
+                            mti = gom.mti.down[, focal.group[iscene]])
+  } else {
+    scene.out <- data.table(Group = out.order, mti = gom.mti[, focal.group[iscene]])
+  }
   
-  #Convert to prop
-  qpress.results[, Value := (Pos - Neg) / 1000]
+  #Load QPress results
+  for(imod in 1:5){
+    mod.name <- paste0('GOMresults', imod, scenarios[iscene], '.csv')
+    qpress.results <- as.data.table(read.csv(file.path(qpress.dir, mod.name)))
+    setnames(qpress.results, c('X', 'X.', 'X0', 'X..1'), c('Group', 'Neg', 'Neu', 'Pos'))
+    
+    #Convert to prop
+    qpress.results[, Value := (Pos - Neg) / 1000]
+    
+    mod.results <- qpress.results[, list(Group, Value)]
+    setnames(mod.results, 'Value', paste0('Model', imod))
+    
+    scene.out <- merge(scene.out, mod.results, by = 'Group', all.x = T)
+  }
   
-  scene.results <- qpress.results[, list(Group, Value)]
-  setnames(scene.results, 'Value', paste0('Model', iscene))
+  #Fix NAs
+  scene.out[is.na(scene.out)] <- 0
   
-  gom.phyto.up <- merge(gom.phyto.up, scene.results, by = 'Group', 
-                        all.x = T)
+  #Set order
+  scene.out[, out.order := factor(Group, levels = out.order)]
+  setorder(scene.out, out.order)
+  
+  #Drop group column
+  scene.out[, c('Group', 'out.order') := NULL]
+  
+  #Plot output
+  png(file = file.path(out.dir, paste0('GOM_', scenarios[iscene], '.png')), 
+      height = 1500, width = 2000, res = 200) 
+  par(mar = c(10, 6, 4, 2))
+  bars <- barplot(as.matrix(t(scene.out)), beside = T, 
+                  col = c('#7fc97f','#beaed4','#fdc086','#ffff99',
+                          '#386cb0','#f0027f'))
+  text(cex = .8, x = colMeans(bars) - .25, y = -1.1, labs, 
+       xpd = NA, srt = 90, adj = 1)
+  abline(v = bars[6, ] + 1, lty = 2, col = 'lightgrey')
+  legend(97, 1.2, legend = c('MTI', '10%', '20%', '30%', '40%', '50%'),
+         fill = c('#7fc97f','#beaed4','#fdc086','#ffff99', '#386cb0',
+                  '#f0027f'), xpd = NA, xjust = .5, horiz = T)
+  
+  dev.off()
 }
 
-#Fix NAs
-gom.phyto.up[is.na(gom.phyto.up)] <- 0
+    
 
-#Set order
-gom.phyto.up[, out.order := factor(Group, levels = out.order)]
-setorder(gom.phyto.up, out.order)
 
-#Drop group column
-gom.phyto.up[, c('Group', 'out.order') := NULL]
 
-png(file = file.path(out.dir, 'GOM_Phyto_up_2.png'), 
-    height = 1500, width = 2000, res = 200) 
-par(mar = c(10, 6, 4, 2))
-bars <- barplot(as.matrix(t(gom.phyto.up)), beside = T, 
-                col = c('#7fc97f','#beaed4','#fdc086','#ffff99',
-                        '#386cb0','#f0027f'))
-text(cex = .8, x = colMeans(bars) - .25, y = -1.1, labs, 
-     xpd = NA, srt = 90, adj = 1)
-abline(v = bars[6, ] + 1, lty = 2, col = 'lightgrey')
-legend(97, 1.2, legend = c('MTI', '10%', '20%', '30%', '40%', '50%'),
-       fill = c('#7fc97f','#beaed4','#fdc086','#ffff99', '#386cb0',
-               '#f0027f'), xpd = NA, xjust = .5, horiz = T)
 
-dev.off()
+
+
 
